31d7baed95ce89e7c534780dbc9ddd9f2c29c92b
Kelly Gemmill <54822281+kellygemmill@users.noreply.github.com>
2021-02-24 00:22:32 -0800

    Updating navigation bar

--- sudoku-frontend/src/components/NavigationBar.js
+++ sudoku-frontend/src/components/NavigationBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Navbar, NavDropdown, Nav } from 'react-bootstrap'
-// import LanguageSelector from 'components/LanguageSelector'
+import LanguageSelector from 'components/LanguageSelector'
 import { Text } from 'containers/Language';
 
 const NavigationBar = () => {
@@ -13,9 +13,12 @@ const NavigationBar = () => {
                 <Nav className="mr-auto">
                     <Nav.Link href='https://kellygemmill.com'><Text textId='home' /></Nav.Link>
                     <NavDropdown title={Text({'textId': 'projects'})} id='basic-nav-dropdown'>
+                        <NavDropdown.Item href='https://kellygemmill.com/#projects'><Text textId='summary' /></NavDropdown.Item>
+                        <NavDropdown.Divider />
                         <NavDropdown.Item href='https://dictionary.kellygemmill.com'><Text textId='dictionary' /></NavDropdown.Item>
                         <NavDropdown.Item href='https://sudokusolver.kellygemmill.com'><Text textId='sudoku' /></NavDropdown.Item>
                     </NavDropdown>
+                    <Nav.Link href='https://kellygemmill.com/#contact-me'><Text textId='contact' /></Nav.Link>
                     <Nav.Link href='https://github.com/kellygemmill' target="_blank" rel="noopener noreferrer"><Text textId='github' /></Nav.Link>
                 </Nav>
             </Navbar.Collapse>
